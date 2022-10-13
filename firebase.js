d6ee5e9d76d4eb4d1d6eebef86e98e25e25d85b9
Leo <leoc2628@gmail.com>
2022-10-13 05:38:35 -0400

    add firebase config

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB27SWzAGXWA_W7s3u0_3Ysfgjl6fx_Ezw",
+  authDomain: "nextjs-netflix-clone-3eeb3.firebaseapp.com",
+  projectId: "nextjs-netflix-clone-3eeb3",
+  storageBucket: "nextjs-netflix-clone-3eeb3.appspot.com",
+  messagingSenderId: "419466626043",
+  appId: "1:419466626043:web:8e0d5e5373f54e57fcc15e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

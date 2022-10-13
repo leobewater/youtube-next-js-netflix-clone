f34c70d59e04d746c6c6a2158c5eab81d4daf1e7
Leo <leoc2628@gmail.com>
2022-10-13 06:02:55 -0400

    set up nav css

--- src/Nav.js
+++ src/Nav.js
@@ -3,8 +3,11 @@ import './Nav.css'
 
 function Nav() {
   return (
-    <div className="nav">
-        This is the nav
+    <div className="nav nav__black">
+        <div className="nav__contents">
+            <img className="nav__logo" src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456" alt="" />
+            <img className="nav__avatar" src="https://i.pinimg.com/originals/8d/ff/49/8dff49985d0d8afa53751d9ba8907aed.png" alt="" />
+        </div>
     </div>
   )
 }

7dc094ec387779955d95373d24f5aa55593b92e0
Leo <leoc2628@gmail.com>
2022-10-13 08:18:35 -0400

    set up signup screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginScreen.css';
+import SignupScreen from './SignupScreen';
 
 function LoginScreen() {
+  const [signIn, setSignIn] = useState(false);
+
   return (
     <div className="login-screen">
       <div className="login-screen__background">
@@ -10,17 +13,40 @@ function LoginScreen() {
           src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456"
           alt=""
         />
-        <button className="login-screen__button">Sign In</button>
+        <button
+          onClick={() => setSignIn(true)}
+          className="login-screen__button"
+        >
+          Sign In
+        </button>
 
-        <div className='login-screen__gradient' />
+        <div className="login-screen__gradient" />
       </div>
 
       <div className="login-screen__body">
-        <>
-        <h1>Unlimited movies, TV programmes and more.</h1>
-        <h2>Watch anywhere. Cancel at any time.</h2>
-        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
-        </>
+        {signIn ? (
+          <SignupScreen />
+        ) : (
+          <>
+            <h1>Unlimited movies, TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+            <div className="login-screen__input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="login-screen__get-started"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SignupScreen.css'
+
+function SignupScreen() {
+  return (
+    <div className='signup-screen'>SignupScreen</div>
+  )
+}
+
+export default SignupScreen

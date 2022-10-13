a661ad160657d12342a973adf79e7c2f0c4b39b9
Leo <leoc2628@gmail.com>
2022-10-13 08:27:52 -0400

    add signup form style

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import './SignupScreen.css'
+import React from 'react';
+import './SignupScreen.css';
 
 function SignupScreen() {
   return (
-    <div className='signup-screen'>SignupScreen</div>
-  )
+    <div className="signup-screen">
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit">Sign In</button>
+        <h4>
+          <span className="signup-screen__gray">New to Netflix? </span>
+          <span className="signup-screen__link">Sign up now.</span>
+        </h4>
+      </form>
+    </div>
+  );
 }
 
-export default SignupScreen
+export default SignupScreen;

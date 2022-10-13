2b24f442edbd22c24591460fc007bc083aaa9f12
Leo <leoc2628@gmail.com>
2022-10-13 07:45:46 -0400

    add 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import ErrorScreen from './screens/ErrorScreen';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomeScreen />, errorElement: <ErrorScreen /> },
+  { path: '/profile', element: <HomeScreen /> },
+]);
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <RouterProvider router={router} />
     </div>
   );
 }
